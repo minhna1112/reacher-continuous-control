bfc620ee887762061e797b422b9643e52573e38d
minhna4 <minhna4@fpt.com>
2023-11-25 22:55:22 +0700

    update download script

--- download_env.sh
+++ download_env.sh
@@ -1,2 +1,2 @@
-wget https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/one_agent/Reacher_Linux.zip
+wget https://s3-us-west-1.amazonaws.com/udacity-drlnd/P2/Reacher/one_agent/Reacher_Linux_NoVis.zip
 unzip Reacher_Linux.zip
